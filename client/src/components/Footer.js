3854aa94735e36c58b3975157fa153d179df2ef2
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-15 15:44:10 +0100

    footer amended to have names

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react
 
 const Footer = () => {
-    return <h1>Hello World</h1>
+    return <p>This app was coded by Moath Al-Zoubi, Jack Carmichael, Sandy Yu, and Kat Zdienkowska </p>
 }
 
 export default Footer;
