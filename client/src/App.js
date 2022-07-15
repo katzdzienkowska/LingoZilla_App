5f241ec2ae960c82968d0066be2cda5c3eceb83d
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-15 22:07:00 +0100

    feedback form setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import LingoZillaContainer from "./container/LingoZillaContainer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import FeedBackForm from "./components/FeedBackForm";
 
 function App() {
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  const addFeedback = (feedback) => {
+    let temp = feedbacks.map((f) => f);
+    temp.push(feedback);
+    setFeedbacks(temp);
+  };
+
   return (
     <div>
       <Header />
 
       <LingoZillaContainer />
-
+      <FeedBackForm addFeedback={addFeedback} />
       <Footer />
     </div>
   );
