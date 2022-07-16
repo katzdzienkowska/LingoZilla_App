31d62dfb101929b0786aa2417aa8c387e69a3f44
Kat Zdzienkowska <katzdzienkowska@kats-mbp.home>
2022-07-16 18:35:38 +0100

    images imported to data.js

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,74 +1,84 @@
-// images to be imported here from images client/src/images
+import horse_img from '../images/horse.png'
+import sheep_img from '../images/sheep.png'
+import cat_img from '../images/cat.png'
+import rooster_img from '../images/rooster.png'
+import rabbit_img from '../images/rabbit.png'
+import turkey_img from '../images/turkey.png'
+import cow_img from '../images/cow.png'
+import pig_img from '../images/pig.png'
+import dog_img from '../images/dog.png'
+
+
 
 const lessonOne = [
     {
-        id: '1_horl1', animal: 'Horse', translation: 'Koń', image: pasteImageObjectFromAbove
+        id: '1_horl1', animal: 'Horse', translation: 'Koń', image: horse_img
     },
     {
-        id: '2_shel1', animal: 'Sheep', translation: 'Owca', image: pasteImageObjectFromAbove
+        id: '2_shel1', animal: 'Sheep', translation: 'Owca', image: sheep_img
     },
     {
-        id: '3_Catl1', animal: 'Cat', translation: 'Kot', image: pasteImageObjectFromAbove
+        id: '3_Catl1', animal: 'Cat', translation: 'Kot', image: cat_img
     },
     {
-        id: '4_Rool1', animal: 'Rooster', translation: 'Kogut', image: pasteImageObjectFromAbove
+        id: '4_Rool1', animal: 'Rooster', translation: 'Kogut', image: rooster_img
     },
     {
-        id: '5_Rabl1', animal: 'Rabbit', translation: 'Królik', image: pasteImageObjectFromAbove
+        id: '5_Rabl1', animal: 'Rabbit', translation: 'Królik', image: rabbit_img
     },
     {
-        id: '6_Turl1', animal: 'Turkey', translation: 'Indyk', image: pasteImageObjectFromAbove
+        id: '6_Turl1', animal: 'Turkey', translation: 'Indyk', image: turkey_img
     },
     {
-        id: '7_Cowl1', animal: 'Cow', translation: 'Krowa', image: pasteImageObjectFromAbove
+        id: '7_Cowl1', animal: 'Cow', translation: 'Krowa', image: cow_img
     },
     {
-        id: '8_Pigl1', animal: 'Pig', translation: 'Świnia', image: pasteImageObjectFromAbove
+        id: '8_Pigl1', animal: 'Pig', translation: 'Świnia', image: pig_img
     },
     {
-        id: '9_Dogl1', animal: 'Dog', translation: 'Pies', image: pasteImageObjectFromAbove
+        id: '9_Dogl1', animal: 'Dog', translation: 'Pies', image: dog_img
     }
 ];
 
 const lessonTwo = [
     {
-        id: '1_Brol2', animal: 'Horse', colour: 'Brown', translation: 'Brązowy', image: pasteImageObjectFromAbove
+        id: '1_Brol2', animal: 'Horse', colour: 'Brown', translation: 'Brązowy', image: horse_img
     },
     {
-        id: '2_Ginl2', animal: 'Cat', colour: 'Ginger', translation: 'Rudy', image: pasteImageObjectFromAbove
+        id: '2_Ginl2', animal: 'Cat', colour: 'Ginger', translation: 'Rudy', image: cat_img
     },
     {
-        id: '3_Mull2', animal: 'Rooster', colour: 'Multicoloured', translation: 'Kolorowy', image: pasteImageObjectFromAbove
+        id: '3_Mull2', animal: 'Rooster', colour: 'Multicoloured', translation: 'Kolorowy', image: rooster_img
     },
     {
-        id: '4_Grel2', animal: 'Rabbit', colour: 'Grey', translation: 'Szary', image: pasteImageObjectFromAbove
+        id: '4_Grel2', animal: 'Rabbit', colour: 'Grey', translation: 'Szary', image: rabbit_img
     },
     {
-        id: '5_Blal2', animal: 'Turkey', colour: 'Black', translation: 'Czarny', image: pasteImageObjectFromAbove
+        id: '5_Blal2', animal: 'Turkey', colour: 'Black', translation: 'Czarny', image: turkey_img
     },
     {
-        id: '6_Pinl2', animal: 'Pig', colour: 'Pink', translation: 'Różowy', image: pasteImageObjectFromAbove
+        id: '6_Pinl2', animal: 'Pig', colour: 'Pink', translation: 'Różowy', image: pig_img
     },
 
 ];
 
 const lessonThree = [
     {
-        id: '1_Horl3', animal: 'Horse', sentence1:'This is a horse.', sentence1Tra: 'To jest koń.', sentence2:'This horse is brown.', sentence2Tra: 'Ten koń jest brązowy.', image: pasteImageObjectFromAbove
+        id: '1_Horl3', animal: 'Horse', sentence1:'This is a horse.', sentence1Tra: 'To jest koń.', sentence2:'This horse is brown.', sentence2Tra: 'Ten koń jest brązowy.', image: horse_img
     },
     {
-        id: '2_Catl3', animal: 'Cat', sentence1:'This is a cat.', sentence1Tra: 'To jest kot.', sentence2:'This cat is ginger.', sentence2Tra: 'Ten kot jest rudy.', image: pasteImageObjectFromAbove
+        id: '2_Catl3', animal: 'Cat', sentence1:'This is a cat.', sentence1Tra: 'To jest kot.', sentence2:'This cat is ginger.', sentence2Tra: 'Ten kot jest rudy.', image: cat_img
     },
     {
-        id: '3_Rool3', animal: 'Rooster', sentence1:'This is a rooster.', sentence1Tra: 'To jest kogut.', sentence2:'This rooster is multicoloured.', sentence2Tra: 'Ten kogut jest kolorowy.', image: pasteImageObjectFromAbove
+        id: '3_Rool3', animal: 'Rooster', sentence1:'This is a rooster.', sentence1Tra: 'To jest kogut.', sentence2:'This rooster is multicoloured.', sentence2Tra: 'Ten kogut jest kolorowy.', image: rooster_img
     },
     {
-        id: '4_Rabl3', animal: 'Rabbit', sentence1:'This is a rabbit.', sentence1Tra: 'To jest królik.', sentence2:'This rabbit is grey.', sentence2Tra: 'Ten królik jest szary.', image: pasteImageObjectFromAbove
+        id: '4_Rabl3', animal: 'Rabbit', sentence1:'This is a rabbit.', sentence1Tra: 'To jest królik.', sentence2:'This rabbit is grey.', sentence2Tra: 'Ten królik jest szary.', image: rabbit_img
     },
     {
-        id: '5_Turl3', animal: 'Turkey', sentence1:'This is a turkey.', sentence1Tra: 'To jest indyk.', sentence2:'This turkey is black.', sentence2Tra: 'Ten indyk jest czarny.', image: pasteImageObjectFromAbove
+        id: '5_Turl3', animal: 'Turkey', sentence1:'This is a turkey.', sentence1Tra: 'To jest indyk.', sentence2:'This turkey is black.', sentence2Tra: 'Ten indyk jest czarny.', image: turkey_img
     },
     {
-        id: '6_Pigl3', animal: 'Pig', sentence1:'This is a pig.', sentence1Tra: 'To jest świnia.', sentence2:'This pig is pink.', sentence2Tra: 'Ta świnia jest różowa.', image: pasteImageObjectFromAbove
+        id: '6_Pigl3', animal: 'Pig', sentence1:'This is a pig.', sentence1Tra: 'To jest świnia.', sentence2:'This pig is pink.', sentence2Tra: 'Ta świnia jest różowa.', image: pig_img
     },
 ];
