c48ce8231873cb917bfbc14040b376dc1bffee23
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-17 16:30:27 +0100

    game2 data pulled in

--- client/src/container/LingoZillaContainer.js
+++ client/src/container/LingoZillaContainer.js
@@ -1,22 +1,15 @@
-import React from 'react' 
-import Header from '../components/Header';
-import Lesson1 from '../components/Lesson1';
+import React from "react";
+import Game2 from "../components/Game2";
+import Header from "../components/Header";
+import Lesson1 from "../components/Lesson1";
 
-const LingoZilla = () => { 
+const LingoZilla = () => {
+  return (
+    <section>
+      <Lesson1 />
+      <Game2 />
+    </section>
+  );
+};
 
-
-
-
-
-
-
-    return (
-        <section>
-            <Lesson1 />
-        </section>
-    )
-
-}
-
-
-export default LingoZilla; 
+export default LingoZilla;
