e685ea11fabf11451d4cca3fed9a1f8f5608ebbe
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-18 11:07:45 +0100

    get feedback set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import LingoZillaContainer from "./container/LingoZillaContainer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FeedBackForm from "./components/FeedBackForm";
 import Game2 from "./components/Game2";
+import FeedBackPage from "./components/FeedBackPage";
+import { getFeedback } from "./service/LingozillaService";
 
 function App() {
   const [feedbacks, setFeedbacks] = useState([]);
+
+  useEffect(() => {
+    getFeedback().then((feedbacks) => setFeedbacks(feedbacks));
+  }, []);
 
   const addFeedback = (feedback) => {
     let temp = feedbacks.map((f) => f);
@@ -21,6 +27,7 @@ function App() {
 
       <LingoZillaContainer />
       <FeedBackForm addFeedback={addFeedback} />
+      <FeedBackPage feedbacks={feedbacks} />
       <Footer />
     </main>
   );
