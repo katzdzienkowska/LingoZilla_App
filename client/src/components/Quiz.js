7513f5e83970505de116d50e6a64f326a243be5e
user <user@users-MBP.lan>
2022-07-18 18:55:01 +0100

    current questions

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -101,33 +101,37 @@ const Quiz = () => {
                     <h2>
                         {score} out of {questions.length} correct - (
                         {(score / questions.length) * 100} %)
-                        <h2>
-                            <button onClick={() => restartGame()}>Restart game</button>
-                        </div>
-                        ) : (
-                        <div className='question-card'>
-                            <h2>
-                                Question: {currentQuestion + 1} out of {questions.length}
-                            </h2>
-                            <h3 className='question-text'>{questions[currentQuestion].text}</h3>
-
-
-                            )
                     </h2>
-                    )
-                </h2>
+                    <button onClick={() => restartGame()}>Restart game</button>
+                </div>
+            ) : (
+                <div className='question-card'>
+                    <h2>
+                        Question: {currentQuestion + 1} out of {questions.length}
+                    </h2>
+                    <h3 className='question-text'>{questions[currentQuestion].text}</h3>
+                    <ul>
+                        {questions[currentQuestion].options.map((option) => {
+                            return (
+                                <li
+                                    key={option.id}
+                                    onClick={() => optionClicked(option.isCorrect)} >
+                                    {option.text}
+                                </li>
+                            );
+                        })};
+                    </ul>
+                </div>
             )}
-
-            <p>
-
-            </p>
-
         </div>
     )
-
-
-
 }
+
+
+
+
+
+
 
 
 export default Quiz;
