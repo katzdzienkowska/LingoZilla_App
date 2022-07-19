d616fa2b5915476d06163ef79c6edd3f597d7035
user <user@users-MBP.lan>
2022-07-19 11:35:02 +0100

    useEffect

--- client/src/components/BackToTopButton.js
+++ client/src/components/BackToTopButton.js
@@ -4,9 +4,29 @@ function BackToTopButton() {
     const [backToTopButton, setBackToTopButton] = useState(false);
 
     useEffect(() => {
-        window.addEventListener("scroll",)
-    })
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setBackToTopButton(true)
+            } else {
+                setBackToTopButton(false)
+            }
+        })
+    }, [])
 
+    const scrollUp = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+
+        })
+    }
+
+    return <div className="app">
+
+        {backToTopButton && (
+            <button className="scroll" onClick={scrollUp}>^</button>
+        )}
+    </div>
 
 
 
