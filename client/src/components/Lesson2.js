0eaf58763551a21ec4558efdd1629924130622cc
Kat Zdzienkowska <katzdzienkowska@kats-mbp.home>
2022-07-19 15:13:06 +0100

    next button working lesson2

--- client/src/components/Lesson2.js
+++ client/src/components/Lesson2.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import {lessonTwo} from '../data/data';
 import Lesson2Item from './Lesson2Item'
 import Game2 from './Game2'
+import FeedBackForm from './FeedBackForm';
 
 const Lesson2 = ({textToSpeech}) => { 
 
@@ -11,15 +13,43 @@ const Lesson2 = ({textToSpeech}) => {
         );
     });
 
+    const [isShown1, setIsShown1] = useState(false);
+    const [isShown2, setIsShown2] = useState(false);
+
+    const handleClick1 = event => {
+        setIsShown1(current => !current);
+    };
+    
+    const handleClick2 = event => {
+        setIsShown2(current => !current);
+    };
+
     return(
         <>
-            <h1>Lesson Two: Farm Animals</h1>
-            <p>short desc of the lesson to be added</p>
-            {animalsList2}
-            <Game2/>
+            <div>
+                <h1>Lesson Two: Colours</h1>
+                <p>short desc of the lesson to be added</p>
+                {animalsList2}
+                <button onClick={handleClick1}>Game time!</button>
+            </div>
+
+            {isShown1 && (
+            <div>
+                < Game2 />
+                <button onClick={handleClick2}>How was the lesson?</button>
+            </div>
+            )}
+
+            {isShown2 && (
+            <div>
+                <FeedBackForm/>
+                <button >
+                <Link to='/lesson3'>Go to lesson 3</Link>
+                </button>
+            </div>
+            )}
         </>
     );
 };
-
 
 export default Lesson2; 
