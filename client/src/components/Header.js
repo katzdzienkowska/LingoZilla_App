a2506c26d5dcd51c59757a97b0c738131171d189
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-20 16:31:38 +0100

    css for header, footer, navbar done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ import cutezilla from "../images/cutezilla.png";
 const Container = styled.div`
   display: flex;
   justify-content: center;
+  background: #fcda7c;
 `;
 
 const Heading = styled.h1`
